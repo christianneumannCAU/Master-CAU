93fe95deed9a6d1a4db4034e16fd30efaaf5db38
christianneumannCAU <christian.christian.neumann@gmail.com>
2021-04-16 12:17:18 +0200

    update getting_started (FFT and plots)

--- 04_scripts/getting_started.m
+++ 04_scripts/getting_started.m
@@ -1,8 +1,9 @@
-%Startup
-clear all;
-close all;
-clc;
+%% Startup
+clear all;  %remove all variables from current workspace
+close all;  %close all plots
+clc;        %clear all text from command window 
 
+%add and initiate toolboxes
 MAIN = [fileparts(pwd) '\'];
 addpath(genpath(MAIN));
 addpath([userpath '\toolboxes\eeglab_current\']);
@@ -12,13 +13,72 @@ ft_defaults;
 %Change MatLab defaults
 set(0,'defaultfigurecolor',[1 1 1]);
 
-% Go to Current Folder
+% Go to Folder with data 
+%(needs to be in the same folder as the script-folder)
+%(see git for the structure of the folders)
 PATHIN_conv = [MAIN '02_Data' filesep '01_converteddata' filesep 'Andreas_Arndt' filesep];
 cd ([PATHIN_conv])
 indat = dir('*.mat');
-DEPTH = extractBetween({indat.name},'D','F'); % Depth is a cell array containing all depth in one participant folder
+DEPTH = extractBetween({indat.name},'D','F'); % extract Depth from filename
 
-%% read in the data
+%% read in the data + preprocessing
 cfg = [];
+cfg.demean = 'yes';             %remove DC offset
+cfg.hpfilter = 'yes';
+cfg.hpfreq = .5;                %high-pass filter, cutting everything under .5 Hz
+cfg.hpfilttype = 'firws';
+cfg.lpfilter = 'yes';
+cfg.lpfreq = 45;                %low-pass filter, cutting everything over 45 Hz
+cfg.lpfilttype = 'firws';
 cfg.dataset = [PATHIN_conv indat(1).name];
 data = ft_preprocessing(cfg);
+
+%% Option 1: FFT, Hanning taper
+    cfg=[];
+    cfg.method='mtmconvol'; 
+    cfg.output='pow'; % Output parameter
+    cfg.foi=[1:2:30]; % Frequency resolution
+    cfg.toi=[-.5:.01: .5]; % Temporal resolution
+    cfg.t_ftimwin = 7./cfg.foi;
+    cfg.taper = 'hanning'; % Frequency-Adaptive Smoothing
+
+    TFR1=ft_freqanalysis(cfg,data);
+
+%% Option 2: Wavelet
+    cfg=[];
+    cfg.method='wavelet'; % Method: Wavelet Transformation
+    cfg.output='pow'; % Output parameter
+    cfg.foi=[1:2:30]; % Frequency resolution
+    cfg.toi=[-.5:.01: .5]; % Temporal resolution
+    cfg.width = 7;
+    TFR2 = ft_freqanalysis(cfg,data);
+    
+%% plots
+%raw data
+plot(data.time{1,1},data.trial{1,1});
+
+%% for Option 1
+% avarage over all timesamples;
+m1 = mean(TFR1.powspctrm,[3],'omitnan'); 
+plot(TFR1.freq,m1);
+
+% watch specific timesamples
+for v = 1:101
+    subplot(10,11,v);
+    plot(TFR1.freq,TFR1.powspctrm(:,:,v));
+end
+
+%% for Option 2
+% avarage over all timesamples;
+m2 = mean(TFR2.powspctrm,[3],'omitnan'); 
+plot(TFR2.freq,m2);
+
+% watch specific timesamples
+for v = 1:101
+    subplot(10,11,v);
+    plot(TFR2.freq,TFR2.powspctrm(:,:,v));
+end
+
+
+
+
