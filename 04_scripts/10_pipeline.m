3d7af6537d52b4965e1aa872f85b2b390ea1d210
christianneumannCAU <christian.christian.neumann@gmail.com>
2021-06-24 18:13:25 +0200

    added presentation for the Exposé

--- 04_scripts/10_pipeline.m
+++ 04_scripts/10_pipeline.m
@@ -85,25 +85,25 @@ for v = 1:length(indat)
     
     TFR{v}          = ft_freqanalysis(cfg,data_FFT{v});
 
-%     % Plotting Option 1
-% 
-%     m{v} = mean(TFR{v}.powspctrm,[3],'omitnan'); %avarege over time
-% 
-%     %normalize data
-%     for c = 1:length(data_FFT{v}.label)
-%         mnm = min(m{v}(c,:),[],'omitnan')
-%         mxm = max(m{v}(c,:),[],'omitnan')
-%         norm_FFT{v}(c,:) = (m{v}(c,:)- mnm)/(mxm - mnm);
-%     end
-% 
-%     figure; hold;
-%     plot(TFR{v}.freq,norm_FFT{v});
-%     str = ['powerspectrum FFT (Hanning)' ' ' DEPTH(v)];
-%     title(str) ;
-%     xlabel 'Frequency [Hz]';
-%     ylabel 'Power';
-%     lgd = legend(extractAfter(data_FFT{v}.label,10));
-%     lgd.NumColumns = length(data_FFT{v}.label);
+    % Plotting Option 1
+
+    m{v} = mean(TFR{v}.powspctrm,[3],'omitnan'); %avarege over time
+
+    %normalize data
+    for c = 1:length(data_FFT{v}.label)
+        mnm = min(m{v}(c,:),[],'omitnan')
+        mxm = max(m{v}(c,:),[],'omitnan')
+        norm_FFT{v}(c,:) = (m{v}(c,:)- mnm)/(mxm - mnm);
+    end
+
+    figure; hold;
+    plot(TFR{v}.freq,norm_FFT{v});
+    str = ['powerspectrum FFT (Hanning)' ' ' DEPTH(v)];
+    title(str) ;
+    xlabel 'Frequency [Hz]';
+    ylabel 'Power';
+    lgd = legend(extractAfter(data_FFT{v}.label,10));
+    lgd.NumColumns = length(data_FFT{v}.label);
     
 %% Extracting Spikes (Rey, Pedreira & Quiroga, 2015)
     cfg             = [];
