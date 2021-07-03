b795fbff2ea63f61ad61b0004a700df445ccf60a
christianneumannCAU <christian.christian.neumann@gmail.com>
2021-07-03 11:22:11 +0200

    added wrapper for fooof and new script

---
+++ 04_scripts/05_fit_fooof.m
@@ -0,0 +1,9 @@
+settings = [];
+freqs = TFR{1, 1}.freq;
+power_spectrum = m{1,1}(1,:);
+missing = find(isnan(power_spectrum)==1)
+power_spectrum(isnan(power_spectrum))= power_spectrum(missing+1);
+f_range = [1 30];
+return_model = 1
+
+fooof_results = fooof(freqs, power_spectrum , f_range ,settings , return_model)
