5a98e3f7fe14778d51035a311b9f5411b9a1e118
christianneumannCAU <christian.christian.neumann@gmail.com>
2021-07-03 11:50:42 +0200

    updated fit_fooof script

--- 04_scripts/05_fit_fooof.m
+++ 04_scripts/05_fit_fooof.m
@@ -1,9 +1,10 @@
 settings = [];
+settings.peak_width_limits = [1 12]
 freqs = TFR{1, 1}.freq;
 power_spectrum = m{1,1}(1,:);
-missing = find(isnan(power_spectrum)==1)
+missing = find(isnan(power_spectrum)==1);
 power_spectrum(isnan(power_spectrum))= power_spectrum(missing+1);
 f_range = [1 30];
-return_model = 1
+return_model = 1;
 
-fooof_results = fooof(freqs, power_spectrum , f_range ,settings , return_model)
+fooof_results = fooof(freqs, power_spectrum , f_range ,settings , return_model);
