9c41d8ffc5750d7c14386670f0689b2aab6f85bd
christianneumannCAU <christian.christian.neumann@gmail.com>
2021-07-09 17:53:42 +0200

    updated Pipeline

--- 04_scripts/10_pipeline.m
+++ 04_scripts/10_pipeline.m
@@ -87,23 +87,23 @@ for v = 1:length(indat)
     
     m{v} = mean(TFR{v}.powspctrm,[3],'omitnan'); %avarege powerspectrum over time
 
-%     % Plotting FFT
-% 
-%     %normalize data
-%     for c = 1:length(data_FFT{v}.label)
-%         mnm = min(m{v}(c,:),[],'omitnan');
-%         mxm = max(m{v}(c,:),[],'omitnan');
-%         norm_FFT{v}(c,:) = (m{v}(c,:)- mnm)/(mxm - mnm);
-%     end
-% 
-%     figure; hold;
-%     plot(TFR{v}.freq,norm_FFT{v});
-%     str = ['powerspectrum FFT (Hanning)' ' ' DEPTH(v)];
-%     title(str) ;
-%     xlabel 'Frequency [Hz]';
-%     ylabel 'Power';
-%     lgd = legend(extractAfter(data_FFT{v}.label,10));
-%     lgd.NumColumns = length(data_FFT{v}.label);
+    % Plotting FFT
+
+    %normalize data
+    for c = 1:length(data_FFT{v}.label)
+        mnm = min(m{v}(c,:),[],'omitnan');
+        mxm = max(m{v}(c,:),[],'omitnan');
+        norm_FFT{v}(c,:) = (m{v}(c,:)- mnm)/(mxm - mnm);
+    end
+
+    figure; hold;
+    plot(TFR{v}.freq,norm_FFT{v});
+    str = ['powerspectrum FFT (Hanning)' ' ' DEPTH(v)];
+    title(str) ;
+    xlabel 'Frequency [Hz]';
+    ylabel 'Power';
+    lgd = legend(extractAfter(data_FFT{v}.label,10));
+    lgd.NumColumns = length(data_FFT{v}.label);
 
 %% FOOOF
 
